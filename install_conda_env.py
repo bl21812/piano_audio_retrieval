95fabf39c6f8444330588bc7f311f9061150f02a
bl21812 <brian.li21812@gmail.com>
2024-03-18 01:19:46 -0400

    smth

---
+++ install_conda_env.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('Video-LLaMA')
+
+import yaml
+
+# with open('Video-LLaMA/environment.yml') as 
---
+++ play_with_encoders.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append('Video-LLaMA')
+
+import video_llama as video_llama
